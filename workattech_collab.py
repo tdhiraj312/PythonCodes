e8f5d636ae00d76269339a1ae2fee8872ceada0d
tdhiraj312 <tdhiraj312@gmail.com>
2024-12-17 22:47:43 +0530

    Updated file

--- workattech_collab.py
+++ workattech_collab.py
@@ -6,4 +6,13 @@ print(str1[0:2])
 print(str1[-3:])
 print(str1+' '+str2)
 print(str1*2)
+print("size of str1= "+str1+" is: ",len(str1))
+print("size of str2= "+str2+" is: ",len(str2))
+
+list1 = [1,2,'D',12.5,"dhiraj thakur"]
+print(type(list1))
+print(list1[2])
+print(list1[4])
+print(list1[0:3])
+print("length of list is: ",len(list1))
 
